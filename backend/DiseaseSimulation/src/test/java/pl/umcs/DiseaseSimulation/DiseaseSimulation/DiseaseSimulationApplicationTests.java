157e4145463c816209a9873747a69810d768dfc8
Oskar <67687200+GitOskar@users.noreply.github.com>
2021-02-14 18:30:35 +0100

    Initial commit

---
+++ backend/DiseaseSimulation/src/test/java/pl/umcs/DiseaseSimulation/DiseaseSimulation/DiseaseSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.umcs.DiseaseSimulation.DiseaseSimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiseaseSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
